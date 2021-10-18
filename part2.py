ecb43b9119d6b42d1d8e59a1d6ad023cfa0ecdeb
Erik Mohn <erikmoh@stud.ntnu.no>
2021-10-18 15:59:22 +0200

    Cleanup before starting part2 and added gitignore

--- part2.py
+++ part2.py
@@ -1,8 +1,5 @@
+from pprint import pprint 
 from DbConnector import DbConnector
-import constants
-from pathlib import Path
-import os
-
 
 
 class Datahandler:
@@ -10,36 +7,34 @@ class Datahandler:
 
     def __init__(self):
         self.handler = DBhandler()
-        self.datapath = Path(constants.DATA_PATH)
-        self.userpath = Path(str(self.datapath) + r"\Data")
-        self.all_users = os.listdir(self.userpath)  # all users in dataset
-
-        with open(Path(str(self.datapath) + r"\labeled_ids.txt"), 'r') as f:
-            self.labeled_users = f.read().splitlines()  # all users with labels
-
-        self.collections = ["TrackPoint", "Activity", "User"]
 
     def db_close_connection(self):
         self.handler.db_close_connection()
         print("Database connection closed..")
+
+    def print_documents(self, collection_name):
+        self.handler.fetch_documents(collection_name)
+        print()
 
 
 class DBhandler:
     def __init__(self):
         self.connection = DbConnector()
         self.client = self.connection.client
-        self.db = self.connection.db
-        
-        self.collection_names = ["User", "Activity", "TrackPoint"]
-        self.activity_id = 1
-        self.trackpoint_id = 1    
+        self.db = self.connection.db 
+
+    def fetch_documents(self, collection_name):
+        collection = self.db[collection_name]
+        documents = collection.find({})
+        for doc in documents: 
+            pprint(doc)
 
 
 def main():
     program = None
     try:
         program = Datahandler()
-        # program.drop_collections()
+        program.print_documents("Activity")
     except Exception as e:
         print("ERROR: Failed to use database:", e)
     finally:
