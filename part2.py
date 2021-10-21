43442a3b5594dc92adea964f5ba85ab5f49a7d4d
Erik Mohn <erikmoh@stud.ntnu.no>
2021-10-22 01:04:33 +0200

    fix type changes

--- part2.py
+++ part2.py
@@ -11,12 +11,6 @@ class DBhandler:
         self.client = self.connection.client
         self.db = self.connection.db 
 
-    def print_documents(self, collection_name):
-        collection = self.db[collection_name]
-        documents = collection.find({})
-        for doc in documents: 
-            pprint(doc)
-
     def get_num_user(self):
         userCollection = self.db["User"]
         result = userCollection.aggregate([
@@ -224,18 +218,12 @@ class DBhandler:
             {
                 "$group": {
                     "_id": {
-                        "year": { "$year": { 
-                            "$dateFromString": { 
-                                "format": "%Y-%m-%d %H:%M:%S",
-                                "dateString": "$activities.start_date_time"
-                            }
-                        }},
-                        "month": { "$month": { 
-                            "$dateFromString": { 
-                                "format": "%Y-%m-%d %H:%M:%S",
-                                "dateString": "$activities.start_date_time"
-                            }
-                        }}
+                        "year": { "$year": 
+                            "$activities.start_date_time"
+                        },
+                        "month": { "$month": 
+                            "$activities.start_date_time"
+                        }
                     },
                     "count": { "$sum": 1 }
                 }
@@ -263,14 +251,8 @@ class DBhandler:
                     "$cond": [
                         { 
                             "$and": [ 
-                                { "$eq": [ { "$year": { "$dateFromString": { 
-                                    "format": "%Y-%m-%d %H:%M:%S",
-                                    "dateString": "$activities.start_date_time" 
-                                }}}, year ]},
-                                { "$eq": [ { "$month": { "$dateFromString": { 
-                                    "format": "%Y-%m-%d %H:%M:%S",
-                                    "dateString": "$activities.start_date_time" 
-                                }}}, month ]}
+                                { "$eq": [ { "$year": "$activities.start_date_time" }, year ]},
+                                { "$eq": [ { "$month": "$activities.start_date_time" }, month ]}
                             ] 
                         },
                         "$$KEEP",
@@ -287,18 +269,8 @@ class DBhandler:
                         "$sum": {
                             "$divide": [{ 
                                 "$subtract": [
-                                    { 
-                                        "$dateFromString": { 
-                                            "format": "%Y-%m-%d %H:%M:%S",
-                                            "dateString": "$activities.end_date_time"
-                                        }
-                                    },
-                                    { 
-                                        "$dateFromString": { 
-                                            "format": "%Y-%m-%d %H:%M:%S",
-                                            "dateString": "$activities.start_date_time"
-                                        }
-                                    }
+                                    "$activities.end_date_time",
+                                    "$activities.start_date_time"
                                 ]
                             }, 3600000]
                         }
@@ -325,7 +297,7 @@ class DBhandler:
             # get user 112's walking activities
             { 
                 "$match": {
-                    "_id" : f"{user_id}",
+                    "_id" : user_id,
                     "activities.transportation_mode" : "walk"
                 }
             },
@@ -333,10 +305,9 @@ class DBhandler:
             { 
                 "$redact": {
                     "$cond": [
-                        { "$eq": [ { "$year": { "$dateFromString": { 
-                            "format": "%Y-%m-%d %H:%M:%S",
-                            "dateString": "$activities.start_date_time" 
-                        }}}, year ]},
+                        { "$eq": [ { "$year": 
+                            "$activities.start_date_time" 
+                        }, year ]},
                         "$$KEEP",
                         "$$PRUNE"
                     ]
@@ -346,7 +317,7 @@ class DBhandler:
             {
                 "$project": { 
                     "_id": 0, 
-                    "activity_id": { "$toInt": "$activities._id" }
+                    "activity_id": "$activities._id"
                 }
             },
             # merge with trackpoints
@@ -400,7 +371,7 @@ class DBhandler:
             {
                 "$project": { 
                     "_id": 1, 
-                    "activity_id": { "$toInt": "$activities._id" }
+                    "activity_id": "$activities._id"
                 }
             },
             # merge with trackpoints
@@ -465,7 +436,7 @@ class DBhandler:
             {
                 "$project": {
                     "_id": 1,
-                    "activity_id": 1
+                    "activity_id": "$activities._id"
                 }
             },
             # merge with trackpoints
@@ -553,76 +524,76 @@ def main():
 
         """ 1. How many users, activities and trackpoints are there in the dataset (after it is
         inserted into the database). """
-        print("Number of users: ", program.get_num_user())
-        print("Number of activities: ", program.get_num_activity())
-        print("Number of trackpoints: ", program.get_num_trackpoint(), '\n')
+        #print("Number of users: ", program.get_num_user())
+        #print("Number of activities: ", program.get_num_activity())
+        #print("Number of trackpoints: ", program.get_num_trackpoint(), '\n')
 
         """ 2. Find the average, minimum and maximum number of activities per user. """
-        avg_activity_for_all_users = program.get_avg_activities_for_user()
-        print("Avrage activities for all users: ", avg_activity_for_all_users, "≈", round(avg_activity_for_all_users, 1))
-        print("Maximum number of activities: ", program.get_max_activities_for_user())
-        print("Minimum number of activities: ", program.get_min_activities_for_user())
+        #avg_activity_for_all_users = program.get_avg_activities_for_user()
+        #print("Avrage activities for all users: ", avg_activity_for_all_users, "≈", round(avg_activity_for_all_users, 1))
+        #print("Maximum number of activities: ", program.get_max_activities_for_user())
+        #print("Minimum number of activities: ", program.get_min_activities_for_user())
 
         """ 3. Find the top 10 users with the highest number of activities. """
-        print("Top 10 users with the highest number of activities:")
-        headers = ["User id", "Number of activities"]
-        print(tabulate(program.get_top_10_users_with_most_activities(), headers=headers), '\n')
+        #print("Top 10 users with the highest number of activities:")
+        #headers = ["User id", "Number of activities"]
+        #print(tabulate(program.get_top_10_users_with_most_activities(), headers=headers), '\n')
 
         """ 4. Find the number of users that have started the activity in one day and ended
         the activity the next day. """
-        print("Number of users that have an activity that ended the day after it started: ", program.ended_activity_at_the_next_day())
+        #print("Number of users that have an activity that ended the day after it started: ", program.ended_activity_at_the_next_day())
 
         """ 5. Find activities that are registered multiple times. You should find the query
         even if you get zero results. """
-        pprint(program.get_same_activities())
+        #pprint(program.get_same_activities())
 
         """ 6. An infected person has been at position (lat, lon) (39.97548, 116.33031) at
         time ‘2008-08-24 15:38:00’. Find the user_id(s) which have been close to this
         person in time and space (pandemic tracking). Close is defined as the same
         minute (60 seconds) and space (100 meters). (This is a simplification of the
         “unsolvable” problem given i exercise 2). """
-        print("The number of users which have been close to each other:")
-        pprint(program.get_number_of_close_users())
+        #print("The number of users which have been close to each other:")
+        #pprint(program.get_number_of_close_users())
 
         """ 7. Find all users that have never taken a taxi. """
-        print("Users that have never taken a taxi:")
-        print(program.find_users_with_no_taxi(), '\n')
+        #print("Users that have never taken a taxi:")
+        #print(program.find_users_with_no_taxi(), '\n')
 
         """ 8. Find all types of transportation modes and count how many distinct users that 
         have used the different transportation modes. Do not count the rows where the
         transportation mode is null. """
-        print("Number of distinct users that have used the different transportation modes:")
-        headers = ["Transportation mode", "Number of users"]
-        print(tabulate(program.count_users_per_transport_mode(), headers=headers), '\n')
+        #print("Number of distinct users that have used the different transportation modes:")
+        #headers = ["Transportation mode", "Number of users"]
+        #print(tabulate(program.count_users_per_transport_mode(), headers=headers), '\n')
 
         """ 9. a) Find the year and month with the most activities. """
-        print("The year and month with the most activities:")
-        print(program.find_date_with_most_activities(), '\n')
+        #print("The year and month with the most activities:")
+        #print(program.find_date_with_most_activities(), '\n')
 
         """ 9. b) Which user had the most activities this year and month, and how many
         recorded hours do they have? Do they have more hours recorded than the user
         with the second most activities? """
-        print("The two users with the most activities that year and month:")
-        headers = ["User id", "Number of activities", "Hours recorded"]
-        print(tabulate(program.find_user_with_most_activities(), headers=headers), '\n')
+        #print("The two users with the most activities that year and month:")
+        #headers = ["User id", "Number of activities", "Hours recorded"]
+        #print(tabulate(program.find_user_with_most_activities(), headers=headers), '\n')
 
         """ 10. Find the total distance (in km) walked in 2008, by user with id=112. """
-        print("Total distance walked by user 112 in 2008:")
-        print(program.find_distance_walked_in_year_by_user(2008, 112), "km\n")
+        #print("Total distance walked by user 112 in 2008:")
+        #print(program.find_distance_walked_in_year_by_user(2008, 112), "km\n")
 
         """ 11. Find the top 20 users who have gained the most altitude meters.
             Output should be a table with (id, total meters gained per user).
             Remember that some altitude-values are invalid
             Tip: (tpn.altitude-tpn-1.altitude), tpn.altitude >tpn-1.altitude """
-        print("Top 20 users who have gained the most altitude meters:")
-        headers=["User id", "Altitude gained"]
-        print(tabulate(program.find_20_users_with_most_altitude_gain(), headers=headers), '\n')
+        #print("Top 20 users who have gained the most altitude meters:")
+        #headers=["User id", "Altitude gained"]
+        #print(tabulate(program.find_20_users_with_most_altitude_gain(), headers=headers), '\n')
 
         """ 12. Find all users who have invalid activities, and the number of invalid activities 
         per user. An invalid activity is defined as an activity with consecutive trackpoints
         where the timestamps deviate with at least 5 minutes. """
-        print("Users with invalid activities: ")
-        pprint(program.find_all_users_with_invalid_activities())
+        #print("Users with invalid activities: ")
+        #print(program.find_all_users_with_invalid_activities())
 
     except Exception as e:
         print("ERROR: Failed to use database:", e)
